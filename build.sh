3bb4b7a7d390688475f0f465172eed0eaf3b9074
Gerard van Helden <gerard@van-helden.name>
2020-08-25 22:40:46 +0200

    add version to build

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 # Builds both Java .jar and the libmv8.{so,dylib} shared library.
 # See BUILDING.md for more info
+
+VERSION=$(git describe)
 
 set -e -x -u
 
@@ -18,15 +21,15 @@ if ! [ -f "${V8_MONOLITH}" ]; then
 	)
 fi
 
-mkdir -p bin
+mkdir -p bin out
 $JAVAC -cp "lib/*" $(find src -name "*.java") -d bin -h src/main/cpp
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	JAVA_INCLUDES="-I$JAVA_HOME/include -I$JAVA_HOME/include/linux"
-	OUTPUT_FILE=libmv8.so
+	OUTPUT_FILE=out/libmv8-$VERSION.so
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	JAVA_INCLUDES="-I$JAVA_HOME/include -I$JAVA_HOME/include/darwin"
-	OUTPUT_FILE=libmv8.dylib
+	OUTPUT_FILE=out/libmv8-$VERSION.dylib
 fi
 
 # Note: Omitting V8_COMPRESS_POINTERS will lead to segfaults
@@ -38,6 +41,6 @@ g++ -shared -I"${V8_INCLUDE}" $JAVA_INCLUDES \
 	-Wl,$V8_MONOLITH \
 	-ldl -pthread -std=c++11 -fPIC
 
-(cd bin && jar cf ../mv8.jar $(find . -name "*.class"))
+(cd bin && jar cf ../out/mv8-$VERSION.jar $(find . -name "*.class"))
 
 echo -e "\n\nDone.\nDon't forget to run \`./test.sh\` to see if everything works.\n"
