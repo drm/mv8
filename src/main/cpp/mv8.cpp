3fe3f70dbf88841164c5214c220bc674f1700fae
Gerard van Helden <gerard@van-helden.name>
2020-10-21 22:52:31 +0200

    fix copyright notice

--- src/main/cpp/mv8.cpp
+++ src/main/cpp/mv8.cpp
@@ -1,13 +1,9 @@
 /*******************************************************************************
-* Copyright (c) 2014 EclipseSource and others.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    EclipseSource - initial API and implementation
-******************************************************************************/
+ * (c) Copyright 2019-2020
+ * Martijn Vogten and Gerard van Helden
+ *
+ * See http://github.com/drm/mv8 for more info
+ ******************************************************************************/
 #include <jni.h>
 #include "libplatform/libplatform.h"
 #include <iostream>
